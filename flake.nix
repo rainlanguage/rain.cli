0d0b26dbbc374cc549998d7a11ab1de77e5da16f
thedavidmeister <thedavidmeister@gmail.com>
2024-01-30 20:21:33 +0400

    add rainix

--- flake.nix
+++ flake.nix
@@ -1,37 +1,17 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rainix.url = "github:rainprotocol/rainix";
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs) {
-          inherit system;
-        };
-
-        naersk' = pkgs.callPackage naersk {};
-
+        pkgs = rainix.pkgs.${system};
       in rec {
-        # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-          nativeBuildInputs = with pkgs; [ pkg-config openssl gmp ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-          ]);
-        };
+        packages = rainix.packages.${system};
 
-        # For `nix develop`:
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo gmp iconv ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-            pkgs.darwin.apple_sdk.frameworks.CoreServices
-          ]);
-        };
+        devShells = rainix.devShells.${system};
       }
     );
 }

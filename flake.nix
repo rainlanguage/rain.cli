661a6b88640a7dc3fedb650cc611372fd0e5d669
thedavidmeister <thedavidmeister@gmail.com>
2024-04-23 20:47:49 +0400

    wip on new build

--- flake.nix
+++ flake.nix
@@ -20,15 +20,32 @@
             cargoLock.lockFile = ./Cargo.lock;
             # allows for git deps to be resolved without the need to specify their outputHash
             cargoLock.allowBuiltinFetchGit = true;
+            # submodules = true;
             buildPhase = ''
+              # mkdir -p $out/lib
+              # cp -r lib $out/lib
+              # ls -la
+              # ls -la lib
+              # cd lib/rain.metadata
+              # mkdir -p $out/cache
+              # mkdir -p $out/out
+              set -euxo pipefail
+              # echo "$src"
+              # ls -la "$src"
+              # ls -la "$src/lib/rain.metadata"
+              # whoami
+              # sudo mkdir -p "$src/out"
+              # forge build --force --root "$src/lib/rain.metadata" --out $src/out
+              # cd -
+              echo $CARGO_TARGET_DIR
               cargo build --release --bin rain
             '';
             installPhase = ''
               mkdir -p $out/bin
               cp target/release/rain $out/bin/
             '';
-            buildInputs = rainix.rust-build-inputs.${system};
-            nativeBuildInputs = rainix.rust-build-inputs.${system};
+            buildInputs = rainix.rust-build-inputs.${system} ++ rainix.sol-build-inputs.${system};
+            nativeBuildInputs = rainix.rust-build-inputs.${system} ++ rainix.sol-build-inputs.${system};
           };
         } // rainix.packages.${system};
 

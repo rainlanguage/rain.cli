835a62a6f8d1ef3ab60a1f9bc56a8a618d425342
thedavidmeister <thedavidmeister@gmail.com>
2024-05-01 22:40:42 +0400

    build lock

--- flake.nix
+++ flake.nix
@@ -16,11 +16,22 @@
             src = pkgs.fetchgit {
               url = "https://github.com/rainlanguage/rain.metadata";
               rev = "b235fb2d4e4d22e3adda4f316f5a7f21164451fb";
-              hash = "sha256-LNBoLu4/TPF++17F6STjiJSpEYlfF2B7dbRhIpcT9EA=";
+              sha256 = "sha256-LNBoLu4/TPF++17F6STjiJSpEYlfF2B7dbRhIpcT9EA=";
             };
             installPhase = ''
               mkdir -p $out
               cp -r . $out
+            '';
+          };
+
+          build-cargo-lock = rainix.mkTask.${system} {
+            name = "build-cargo-lock";
+            body = ''
+              echo $PWD
+              rm -rf ./lib/rain.metadata
+              mkdir -p ./lib/rain.metadata
+              cp --no-preserve=mode,ownership -r ${rain-metadata-repo}/* ./lib/rain.metadata
+              cargo generate-lockfile
             '';
           };
 
@@ -48,13 +59,23 @@
               cp target/release/rain $out/bin/
             '';
             buildInputs = rainix.rust-build-inputs.${system} ++ rainix.sol-build-inputs.${system};
-            nativeBuildInputs = rainix.rust-build-inputs.${system} ++ rainix.sol-build-inputs.${system};
+            nativeBuildInputs = rainix.rust-build-inputs.${system} ++ rainix.sol-build-inputs.${system} ++ [rain-metadata-repo];
           };
         } // rainix.packages.${system};
 
         defaultPackage = packages.rain;
 
-        devShells = rainix.devShells.${system};
+        devShells.default = pkgs.mkShell {
+          packages = [
+            packages.rain
+            packages.rain-metadata-repo
+            packages.build-cargo-lock
+          ];
+
+          shellHook = rainix.devShells.${system}.default.shellHook;
+          buildInputs = rainix.devShells.${system}.default.buildInputs;
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
+        };
       }
     );
 }
